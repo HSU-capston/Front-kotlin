54b0619c8dd67ed5f205c73092c89be21c344efa
Son Juwan <wndhks171@naver.com>
2025-03-23 19:30:44 +0900

    "[Feat/#16] Signin UI + 로직 수정"

--- Capston37/app/src/main/java/com/example/capston_spotyup/Signin/SigninActivity.kt
+++ Capston37/app/src/main/java/com/example/capston_spotyup/Signin/SigninActivity.kt
@@ -4,6 +4,7 @@ import com.example.capston_spotyup.Main.MainActivity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.capston_spotyup.SignupActivity
 import com.example.capston_spotyup.databinding.SigninBinding
 
 
@@ -19,6 +20,10 @@ class SigninActivity : AppCompatActivity() {
         // 로그인 버튼 클릭 시 fragment_home으로 이동
         binding.loginButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.loginButton2.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
     }
