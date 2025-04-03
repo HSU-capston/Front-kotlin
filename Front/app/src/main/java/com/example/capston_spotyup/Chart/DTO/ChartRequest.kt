ca8b55e84ce97246af901f3f4039fe8dae209ef1
Hwang_Junhyun <h010617@naver.com>
2025-04-03 14:52:34 +0900

    api수정중

---
+++ Front/app/src/main/java/com/example/capston_spotyup/Chart/DTO/ChartRequest.kt
@@ -0,0 +1,6 @@
+package com.example.capston_spotyup.Chart.DTO
+
+data class ChartRequest(
+    val userId: Long,    // 사용자 ID
+    val sportsId: Long   // 스포츠 ID
+)
--- Front/app/src/main/java/com/example/capston_spotyup/Signup/Domain/fragmentInfo.kt
+++ Front/app/src/main/java/com/example/capston_spotyup/Signup/Domain/fragmentInfo.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.capston_spotyup.Main.MainActivity
 import com.example.capston_spotyup.R
 import com.example.capston_spotyup.databinding.SignupInfoBinding
 import com.google.android.material.button.MaterialButton
