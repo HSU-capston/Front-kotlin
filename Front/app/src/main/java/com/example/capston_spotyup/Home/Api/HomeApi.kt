83bab34cc470af69d8e5a2e6f7c632b2cc2b9b90
Son Juwan <wndhks171@naver.com>
2025-04-11 14:04:24 +0900

    "[Feat/#53] Home Api 생성"

---
+++ Front/app/src/main/java/com/example/capston_spotyup/Home/Api/HomeApi.kt
@@ -0,0 +1,10 @@
+package com.example.capston_spotyup.Home.Api
+
+import com.example.capston_spotyup.Home.DTO.HomeResponse
+import retrofit2.http.GET
+import retrofit2.Response
+
+interface HomeApi {
+    @GET("/home/recommendations")
+    suspend fun getHomeRecommendations(): Response<HomeResponse>
+}
