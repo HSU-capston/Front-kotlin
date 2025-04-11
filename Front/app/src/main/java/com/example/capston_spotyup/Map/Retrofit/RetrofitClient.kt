a054b63cdd6199bdd0bd235f44b69d4fc630de0e
Son Juwan <wndhks171@naver.com>
2025-04-11 14:29:26 +0900

    "[Feat/#53] list api, friendapi, userapi 추가"

--- Front/app/src/main/java/com/example/capston_spotyup/Map/Retrofit/RetrofitClient.kt
+++ Front/app/src/main/java/com/example/capston_spotyup/Map/Retrofit/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.example.capston_spotyup.Network
 
+import ListApi
 import com.example.capston_spotyup.Analyze.Api.ChartApi
 import com.example.capston_spotyup.Login.Api.LoginApi
 import com.example.capston_spotyup.Main.Api.DatesApi
@@ -7,6 +8,8 @@ import com.example.capston_spotyup.Main.Api.GameApi
 import com.example.capston_spotyup.Main.Api.GameExitApi
 import com.example.capston_spotyup.Main.Api.SportsApi
 import com.example.capston_spotyup.Map.Api.BowlingApi
+import com.example.capston_spotyup.Profile.Api.FriendApi
+import com.example.capston_spotyup.Profile.Api.UserApi
 import com.example.capston_spotyup.Signup.Api.SmsVerificationApi
 import com.example.capston_spotyup.User.Api.EmailApi
 import okhttp3.OkHttpClient
@@ -91,6 +94,22 @@ object RetrofitClient {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(GameApi::class.java)
+    }
+    val listApi: ListApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ListApi::class.java)
+    }
+    val userApi: UserApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UserApi::class.java)
     }
 
     val gameExitApi: GameExitApi by lazy {
