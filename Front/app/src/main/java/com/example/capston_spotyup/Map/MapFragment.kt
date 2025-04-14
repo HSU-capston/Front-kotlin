e1da0b90c05608816deda8c1d3e13e0c26e44399
Son Juwan <wndhks171@naver.com>
2025-04-14 20:57:15 +0900

    "[Feat/#55] Map Api"

--- Front/app/src/main/java/com/example/capston_spotyup/Map/MapFragment.kt
+++ Front/app/src/main/java/com/example/capston_spotyup/Map/MapFragment.kt
@@ -1,17 +1,21 @@
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
+import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
-import com.kakao.vectormap.MapView
-import com.kakao.vectormap.CoordConverter
-import com.kakao.vectormap.LatLng
-
-
-
-
 import com.example.capston_spotyup.databinding.FragmentMapBinding
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.KakaoMapSdk
+import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
+import com.kakao.vectormap.camera.CameraUpdate
+import com.kakao.vectormap.camera.CameraUpdateFactory
+import com.example.capston_spotyup.BuildConfig
+
 
 class MapFragment : Fragment() {
 
@@ -19,29 +23,50 @@ class MapFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var mapView: MapView
+    private var kakaoMap: KakaoMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+        KakaoMapSdk.init(requireContext(), BuildConfig.KAKAO_APP_KEY)
+
 
         // MapView 참조 (XML에 정의된 MapView 사용)
         mapView = binding.mapView
 
+        // 지도 초기화
+        initializeMap()
 
-        // 서울 시청 좌표 (WGS84 좌표계)
-        val latLng = LatLng.from(37.566535, 126.977969)
-
-        // 지도 초기화 및 중심 설정
-//        mapView.setMapCenter(latLng)
-
-        val convertedLatLng = CoordConverter.toLatLngFromWCONG(524612.7, 1082046.6)
-
-        // 좌표 변환 후 사용
-        // 예를 들어, 변환된 좌표로 지도 중심을 설정할 수 있음
-//        mapView.setMapCenter(convertedLatLng)
         return binding.root
+    }
+
+    private fun initializeMap() {
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                // 지도 API가 정상적으로 종료될 때 호출
+                Log.d("KakaoMap", "onMapDestroy")
+            }
+
+            override fun onMapError(error: Exception) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출
+                Log.e("KakaoMap", "onMapError", error)
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(map: KakaoMap) {
+                // 정상적으로 인증이 완료되었을 때 호출
+                // KakaoMap 객체를 얻어 옵니다.
+                kakaoMap = map
+
+                // 서울 시청 좌표(WGS84)
+                val seoulCityHall = LatLng.from(37.566535, 126.977969)
+
+                // CameraUpdate 객체를 사용하여 카메라 이동
+                val cameraUpdate = CameraUpdateFactory.newCenterPosition(seoulCityHall)
+                kakaoMap?.moveCamera(cameraUpdate)
+            }
+        })
     }
 
     override fun onDestroyView() {
