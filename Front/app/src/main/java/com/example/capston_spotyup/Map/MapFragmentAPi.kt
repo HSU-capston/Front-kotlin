1b8d9a11b3f7605587eaab71c8d2074e47e85dc8
Son Juwan <wndhks171@naver.com>
2025-04-26 00:57:21 +0900

    "[Add/#75] Map 예시 파일(미정)"

--- Front/app/src/main/java/com/example/capston_spotyup/Map/MapFragmentAPi.kt
+++ Front/app/src/main/java/com/example/capston_spotyup/Map/MapFragmentAPi.kt
@@ -1,3 +1,14 @@
+package com.example.capston_spotyup.Map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.capston_spotyup.databinding.FragmentMapApiBinding
+import com.example.capston_spotyup.databinding.FragmentMapBinding
+import com.example.capston_spotyup.databinding.FragmentSubBinding
+
 //package com.example.capston_spotyup.Map
 //
 //import com.naver.maps.map.MapFragment
@@ -46,3 +57,24 @@
 //        _binding = null
 //    }
 //}
+
+class MyFragmentAPi : Fragment(){
+    private var _binding: FragmentMapApiBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // ViewBinding을 통해 레이아웃을 반환
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // ViewBinding 객체를 해제하여 메모리 누수를 방지
+        _binding = null
+    }
+
+}
